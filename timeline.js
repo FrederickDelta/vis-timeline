ea5d03f08f9932c7bf978d1c40c7b2110c1469e5
Frederick D <FrederickDelta@gmail.com>
2021-10-19 18:26:55 +0800

    🐛 FIX: link to index page

--- timeline.js
+++ timeline.js
@@ -1,7 +1,14 @@
 const urlParams = new URLSearchParams(window.location.search);
-let endpoint = urlParams.get('tl');
-
 window.history.replaceState({}, document.title, "timeline.html");
+
+const currentUrl = window.location.href;
+const urlStart = currentUrl.slice(0, currentUrl.lastIndexOf("/")+1);
+
+const linkIndex = document.getElementById("link-index");
+linkIndex.href = `${urlStart}index.html`
+
+const endpoint = urlParams.get('tl');
+const urlEnd = endpoint ? endpoint.slice(0, endpoint.lastIndexOf("/")+1): "";
 
 const options = {
   start_at_slide: 0, 
@@ -13,12 +20,10 @@ fetch(endpoint)
   .then(res => res.json())
   .then(data => {
     // Change the base url to the same as json file
-    currentUrl = window.location.href;
-    let urlStart = currentUrl.slice(0, currentUrl.lastIndexOf("/")+1);
-    let urlEnd = endpoint.slice(0, endpoint.lastIndexOf("/")+1);
     document.head.innerHTML = document.head.innerHTML + `<base href="${urlStart}${urlEnd}" />`;
 
     window.timeline = new TL.Timeline('timeline-embed', data, options);
+  })
+  .catch((error) => {
+    console.error('Error123:', error);
   });
-
-  
