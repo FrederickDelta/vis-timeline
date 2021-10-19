d33fef392ba6eaadc2c01f5588e5d4741a7abbe3
Frederick D <FrederickDelta@gmail.com>
2021-10-19 17:38:15 +0800

    🐛 FIX: Update base url to load images

--- timeline.js
+++ timeline.js
@@ -7,8 +7,18 @@ const options = {
   start_at_slide: 0, 
   // initial_zoom: 4,
   hash_bookmark: false
-}
+};
 
 fetch(endpoint)
   .then(res => res.json())
-  .then(data => window.timeline = new TL.Timeline('timeline-embed', data, options))
+  .then(data => {
+    // Change the base url to the same as json file
+    currentUrl = window.location.href;
+    let urlStart = currentUrl.slice(0, currentUrl.lastIndexOf("/")+1);
+    let urlEnd = endpoint.slice(0, endpoint.lastIndexOf("/")+1);
+    document.head.innerHTML = document.head.innerHTML + `<base href="${urlStart}${urlEnd}" />`;
+
+    window.timeline = new TL.Timeline('timeline-embed', data, options);
+  });
+
+  
